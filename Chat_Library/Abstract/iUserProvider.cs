6b5216a8123226e3a4ffb32f876b86918df1f4c3
arikatamo25 <Arikatamo@DESKTOP-126N0RJ>
2018-02-27 12:24:03 +0200

    iUserProvider

---
+++ Chat_Library/Abstract/iUserProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat_Library.Abstract
+{
+    interface iUserProvider
+    {
+        string GetName(int id);
+        DateTime GetDateCreate(int id);
+        int GetId(string name);
+        IList<tblUser> GetAllUsers();
+        tblUser AddUser(object name);
+    }
+}
